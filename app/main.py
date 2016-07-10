c537f502c99c6cf2b47fedb27cb4abc829d0136a
fanglihui1992@126.com <fanlihui1992@126.com>
2016-07-10 21:10:29 +0800

    add the personInfo

--- app/main.py
+++ app/main.py
@@ -15,13 +15,13 @@ from dbSetting import create_app,db
 
 from route.loginRegister import loginRegister_route
 from route.publishFood import publishFood_route
-
+from route.personInfo import personInfo_route
 app = create_app()
 
 ##注册蓝本路由
 app.register_blueprint(loginRegister_route)  			#注册与登录
 app.register_blueprint(publishFood_route)
-
+app.register_blueprint(personInfo_route)
 
 if __name__ == '__main__':
 	app.run(host=os.getenv('IP','0.0.0.0'),port=int(os.getenv('PORT',8080)),debug = True)
