9cc29030fccc08e30ddfa81e52ee7e92fe021e10
iThinkSeu <yqsseu@163.com>
2016-07-11 09:43:56 +0800

    port change to 5000

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ app.register_blueprint(personInfo_route)
 app.register_blueprint(userInfo_route)
 
 if __name__ == '__main__':
-	app.run(host=os.getenv('IP','0.0.0.0'),port=int(os.getenv('PORT',8080)),debug = True)
+	app.run(host=os.getenv('IP','0.0.0.0'),port=int(os.getenv('PORT',5000)),debug = True)
 
 
 
