b251628f0822b25a757743a3f71a010402049004
zhoury14 <zhoury14@tsinghua.edu.cn>
2016-07-14 03:18:47 +0800

    add userInfo

--- app/route/personInfo.py
+++ app/route/personInfo.py
@@ -32,6 +32,26 @@ def personInfo():
              errorDic = dict(errorDic,**emptyDic)
              return jsonify(errorDic)
 
+        validOrders = user.order.filter(_or('paysate = 6' , 'paystate = 2')).order_by(orderList.paydatetime.desc()).limit(30).all()
+        cancelNum = 0
+        freeNum = 0
+        discountPrice = 0
+        totalPrice = 0
+        number = 0
+        for item in validOrders:
+            number += 1
+            if item.paystate == 4:
+                cancelNum += 1
+            if item.discount == 0:
+                freeNum += 1
+            totalPrice += item.price
+            discountPrice += item.payprice
+
+        customer.friendly = 60 + freeNum / number * 40
+        customer.honesty = 100 - cancelNum / number * 100
+        customer.passion = 60 + (totalPrice - discountPrice) / totalPrice * 40
+        db.session.commit()
+
         friendly = user.friendly
         honesty = user.honesty
         passion = user.passion
