27e2e54ee2a37e5321cf4ac44ab92e6530c8b830
iThinkSeu <yqsseu@163.com>
2016-07-13 22:04:05 +0800

    upload fix

--- app/route/uploadimage.py
+++ app/route/uploadimage.py
@@ -130,6 +130,22 @@ def uploadavatar():
 			elif type == "14":
 				#type = 14 表示滑动图片
 				dst = '/home/www/uploadfiles/shiguang/top/' + 'top'+str(number)+'.jpg'
+			elif type == "15":
+				#type = 15 表示confirm image
+				url = '119.29.233.72:3001/uploadfiles/shiguang/cinfirm/' +str(number)+'.jpg'
+				if u!=None:
+					if number==1:
+						u.cookLifeimgurl = url
+					else if number ==2:
+						u.cookEnvirimgurl = url
+					else:
+						u.bestFoodimgurl = url
+					u.addchange()
+				else:
+					return jsonify({'id':'',
+									'state':'fail',
+									'reason':'no this id'})			
+				dst = '/home/www/uploadfiles/shiguang/cinfirm/' +str(number)+'.jpg'
 			else:
 				state = 'fail'
 				reason = 'no this type'				
